455265ef02356540032d61bf15798a24cb3db2fd
Daniel Padierna <114093359+Dannyboy057@users.noreply.github.com>
2022-11-10 12:30:14 -0600

    Update main.c
    modified the main.c to include comments and now it is able to accept commands with spaces

--- main.c
+++ main.c
@@ -1,42 +1,66 @@
 
+//major2
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>   
-#include <errno.h>    
-#include <sys/wait.h> 
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#define command 50     //50 characters max received per command 
 
-	char *input = NULL;
-    size_t buffer = 0; 
-	int counter;
-	char *token;
-	char *args[];
 
-	int main(int argc, char *argv[]){
-		while(1){
-            printf("major2> ");
-            getline(&input, &buffer, stdin);
-            	if(strcmp(input,"\n")==0){
-					printf("Please type in a command\n" );
-					continue;
-                }
+int main()
+{
+	char input[50];           //create string to store command 
+	char* line;    //pointer to constant 
 
-            token = strtok(input, "\n ");
-			while (token != NULL) { 
-                counter = 0;
-				args[counter++] = token; 
-				token = strtok(NULL, "\n ");
-            	}
-                args[counter] = NULL;
-            
-			if (strcmp(args[0], "quit") == 0) {
-				return 0;
-			}
+	char* args[command];   //a max of 50 arguments
+	int count = 0, pid, i;
+	
+	printf("major2> ");     //initial print line to ask user for a command
+	fgets(input, 100, stdin);      //read a line from a string
 
-            int pid = fork(); 
-            if(pid == 0) // child process
+ 
+    
+	while (strcmp(input, "quit\n"))  //string comparison to continue with loop
+        {                                //while user does not input the command "quit"
+
+		printf("minor3> ");      //ask for command
+
+		count = 0;
+
+		input[strlen(input) - 1] = '\0';    //string length
+
+		for (i = 0; i < 10; i++)
+		{
+			args[i] = (char*)malloc(20 * sizeof(char)); //memory allocation
+		}
+
+		
+
+		line = strtok(input, " ");   //tokenize string adn separate it when space is found
+
+		strcpy(args[count++], line);   //copy every susbstring
+
+ 
+		while (line != "quit")   //do until string is empty
+		{
+			line = strtok(NULL, " ");
+			if (line == NULL)
+				break;
+			
+			strcpy(args[count++], line);
+		}
+
+		//after building list of commands , execute command using execvp , for that create child process
+
+		args[count] = NULL;
+		pid = fork();   //return value of fork()
+
+
+	
+		if(pid == 0) // child process
             {
-                execvp(args[0], args);
+                execvp(args[0], args);      //output if wrong command
                 printf("%s: Command not found\n", args[0]);
                 break;
             }
@@ -46,5 +70,10 @@
             } else {
                 printf("Error: fork() failed!\n");
             }
-        }   
+		
+		
+		
+		fgets(input, 100, stdin); //store string until a new command is given
+	}
+
 }
